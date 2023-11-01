9c946fa3737ec1356446ee05b84918740aa1982f
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-01 15:37:43 -0500

    Estructura inicial del codigo

---
+++ prouno.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    // Se pide que ingrese el usuario el numero con el que se va a trabajar.
+    int n;
+    printf("Escriba su numero : \n");
+    scanf("d%\n", n);
+
+    return 0;
+}
